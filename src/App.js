628915fbc1fb7f2367effc677a5629767d1af6c3
ram-prasad <ram.prasad@inmobi.com>
2024-05-10 20:05:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+
+import { Layout, Menu, theme } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+
+import { Home } from './pages/Home/Home';
+import React from 'react';
+
+const { Header, Content, Footer, Sider } = Layout;
+const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+  (icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }),
+);
+const App = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout style={{'height': '100vh'}}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical">Shrimitra Labs</div>
+        <Menu theme="dark" mode="inline" style={{height: '100%',}} defaultSelectedKeys={['4']} items={items} />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <div className="demo-logo-horizontal">Welcome to the heart of Shrimitra Labs</div>
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px 0',
+            height: '100%',
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Home />
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
